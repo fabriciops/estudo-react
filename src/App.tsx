55fc463eb429011cb3151660aa26d107c167cc84
fabriciops <fabriciopps19@gmail.com>
2022-07-25 23:43:15 -0300

    Botao

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {Header} from './components/Header'
+import {Botao} from './components/Botao'
 
 function App() {
   const [name, setName] = useState('')
-  const [sobrenome, setSobrenome] = useState('')
-  const [idade, setIdade] = useState('')
+  let textButton = 'Clicar no Botão';
 
-
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) =>{
-    console.log(event.target.name);
-    if(event.target.name == 'nome'){
-      setName(event.target.value);
-    }else if(event.target.name == 'sobrenome'){
-      setSobrenome(event.target.value);
-    }else{
-      setIdade(event.target.value);
-    }
+  const botaoEventAction = (txt: string) => {
+    alert("frase do app");
   }
 
   return (
     <div>
-      <input type="text" value={name} onChange={handleInput} name='nome' placeholder='Nome'/>
-      <input type="text" value={sobrenome} onChange={handleInput} name='sobrenome' placeholder='Sobrenome'/>
-      <input type="text" value={idade} onChange={handleInput}  name='idade' placeholder='Idade'/>
-      <hr />
-      Olá  {name} {sobrenome}
-      <hr />
-      
-      Sua Idade  {idade}
-      <hr />
-     
-
+      <Botao text={textButton} clickFn={botaoEventAction} />
     </div>
     
   );
---
+++ src/components/Botao.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    text: string;
+    clickFn: (txt: string) => void;
+}
+
+export const Botao = ({ text, clickFn }: Props) => {
+    const handleClick = () =>{
+        clickFn("frase");
+    }
+    return (
+        <button onClick={handleClick}>{text}</button>
+    );
+};
