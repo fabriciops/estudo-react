2013ce49f69d6f9cf5fdc06915166f4f9e8659b2
fabriciops <fabriciopps19@gmail.com>
2022-10-11 00:33:37 -0300

    calc

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -11,7 +11,7 @@ export const GridItem = ({ item }: Props) => {
     return (
         <div className={styles.main} style={{ backgroundColor: item.color}}>
             <div className={styles.gridIcon}>
-                <img src={item.icon ==='up' ? upImage : downImage} alt='' width="30"/>
+                <img src={item.icon === 'up' ? upImage : downImage} alt='' width="30"/>
             </div>
             <div className={styles.gridTitle}>{item.title}</div>
             <div className={styles.gridInfo}>
